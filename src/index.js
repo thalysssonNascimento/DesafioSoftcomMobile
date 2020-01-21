c877364d8c059f7d97ae6928791f4e224e5d4d23
Thalysson <thalyssonwg.nascimento@gmail.com>
2020-01-21 11:49:58 -0300

    Start project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+// import Routes from './routes';
+// import './config/ReactotronConfig';
+
+export default function App() {
+    return <Text>Test do app</Text>;
+}
